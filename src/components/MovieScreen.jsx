22360819b4660741a6b40da49931809eab7c2f68
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-09-28 18:26:24 -0600

    Movie watchlist pt. 2 complete - Movie text displays

---
+++ src/components/MovieScreen.jsx
@@ -0,0 +1,18 @@
+const MovieScreen = ({ addMovie, movieList, page, setPage, watchlist, removeMovie }) => {
+
+    const movieDisplay = movieList.map((movie, index) => {
+        return <h2>{movie.original_title}</h2>;
+    });
+
+    return (
+        <div className="page">
+            <h1>Brycen's Movie Theatre</h1>
+            <h3>Add a movie to your watchlist!</h3>
+            <div className="movie-container">
+                {movieDisplay}
+            </div>
+        </div>
+    );
+};
+
+export default MovieScreen
