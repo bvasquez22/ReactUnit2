755361e38f76f2f1dc07c4886116421700d73a91
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-10-04 18:53:00 -0600

    Errors fixed and button functionality added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 // import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import Header from './components/Header';
-import MovieScreen from './components/MovieScreen';
-import axios from 'axios';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import MovieScreen from "./components/MovieScreen";
+import Watchlist from "./components/Watchlist";
+import axios from "axios";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
-  const [watchlist, setWatchList] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
   const [page, setPage] = useState(1);
+
+  const addMovie = (movie) => setWatchlist([...watchlist, movie]);
+  const removeMovie = (movie) => {
+    const newState = watchlist.filter((movies) => {
+      return movies !== movie;
+    });
+    setWatchlist(newState);
+  };
 
   const getData = () => {
     axios
@@ -30,11 +39,14 @@ function App() {
       <Header />
       <main>
         <MovieScreen
+          addMovie={addMovie}
+          removeMovie={removeMovie}
           movieList={movieList}
           page={page}
           setPage={setPage}
           watchlist={watchlist}
         />
+        <Watchlist watchlist={watchlist} removeMovie={removeMovie} />
       </main>
     </div>
   );
--- src/components/MovieScreen.jsx
+++ src/components/MovieScreen.jsx
@@ -1,18 +1,43 @@
-const MovieScreen = ({ addMovie, movieList, page, setPage, watchlist, removeMovie }) => {
+import MovieCard from "./MovieCard";
 
-    const movieDisplay = movieList.map((movie, index) => {
-        return <h2>{movie.original_title}</h2>;
-    });
+const MovieScreen = ({
+  addMovie,
+  movieList,
+  page,
+  setPage,
+  watchlist,
+  removeMovie,
+}) => {
+  const decrement = () => {
+    setPage(page - 1);
+  };
 
+  const increment = () => {
+    setPage(page + 1);
+  };
+
+  const movieDisplay = movieList.map((movie, index) => {
     return (
-        <div className="page">
-            <h1>Brycen's Movie Theatre</h1>
-            <h3>Add a movie to your watchlist!</h3>
-            <div className="movie-container">
-                {movieDisplay}
-            </div>
-        </div>
+      <MovieCard
+        movie={movie}
+        addMovie={addMovie}
+        removeMovie={removeMovie}
+        watchlist={watchlist}
+      />
     );
+  });
+
+  return (
+    <div className="page">
+      <h1>Brycen's Movie Theatre</h1>
+      <h3>Add a movie to your watchlist!</h3>
+      <div className="btn-container">
+        <button onClick={page !== 1 && decrement}>Previous</button>
+        <button onClick={increment}>Next</button>
+      </div>
+      <div className="movie-container">{movieDisplay}</div>
+    </div>
+  );
 };
 
-export default MovieScreen
+export default MovieScreen;
